1e497d17ba281c4508883c6663ac7b8619cfb210
Jaime Bünzli <jpbunzli@gmail.com>
2020-04-25 08:23:41 -0400

    feat(register-deposit): command to 'ledgerize' a deposit

---
+++ app/commands/register_deposit.rb
@@ -0,0 +1,24 @@
+class RegisterDeposit < PowerTypes::Command.new(:deposit)
+  include Ledgerizer::Execution::Dsl
+
+  def perform
+    execute_user_deposit_bananas_entry(
+      tenant: casino,
+      document: @deposit,
+      datetime: @deposit.created_at
+    ) do
+      debit(account: :vault, amount: bananas_in_deposit)
+      credit(account: :wallet, accountable: @deposit.user, amount: bananas_in_deposit)
+    end
+  end
+
+  private
+
+  def bananas_in_deposit
+    Money.from_amount(@deposit.bananas, 'BAN')
+  end
+
+  def casino
+    Casino.first
+  end
+end
