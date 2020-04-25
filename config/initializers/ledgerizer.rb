0c16702826f7c82d48f5a608a31469502691d856
Jaime Bünzli <jpbunzli@gmail.com>
2020-04-25 08:25:58 -0400

    feat(ledgerizer): add withdrawal_fees account

--- config/initializers/ledgerizer.rb
+++ config/initializers/ledgerizer.rb
@@ -2,6 +2,9 @@ Ledgerizer.setup do |conf|
   conf.tenant(:casino, currency: :ban) do
     # where the casino keeps the bananas (like a bank account)
     conf.asset :vault
+
+    # casino incomes for withdrawal services
+    conf.income :withdrawal_fees
     # where monkeys keep their betting chips. Is a liability for the casino, because the casino
     # should pay back the equivalent in bananas.
     conf.liability :wallet
