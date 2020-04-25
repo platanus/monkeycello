6b543ff96c6f86b8069ce004374932ea204b4ce5
Jaime Bünzli <jpbunzli@gmail.com>
2020-04-25 08:21:31 -0400

    feat(ledgerizer): add user_deposit_bananas entry

--- config/initializers/ledgerizer.rb
+++ config/initializers/ledgerizer.rb
@@ -6,5 +6,13 @@ Ledgerizer.setup do |conf|
     # should pay back the equivalent in bananas.
     conf.liability :wallet
 
+    # when monkeys exchange bananas for chips
+    conf.entry :user_deposit_bananas, document: :deposit do
+      # increases the amount of bananas in the vault
+      conf.debit account: :vault
+
+      # increases debt of bananas for a specific monkey
+      conf.credit account: :wallet, accountable: :user
+    end
   end
 end
