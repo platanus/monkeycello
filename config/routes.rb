79e54c81c5be0af5cf29251019f1a2cbc2ae36a8
Alejandra Rojas <alejandra@platan.us>
2020-06-03 19:26:23 -0400

    feat(api): power_api generate casino

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-scope path: '/api' do
-api_version(module: 'Api::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
-end
-end
+  scope path: '/api' do
+    api_version(module: 'Api::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
+      resources :casinos
+    end
+  end
+  scope path: '/api' do
+    api_version(module: 'Api::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
+      resources :casinos
+      resources :casinos
+    end
+  end
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
   scope path: '/api' do
     api_version(module: "Api::V1", path: { value: "v1" }, defaults: { format: 'json' }) do
+      resources :casinos
+      resources :casinos
     end
   end
   devise_for :admin_users, ActiveAdmin::Devise.config
---
+++ spec/swagger/v1/definition.rb
@@ -0,0 +1,13 @@
+API_V1 = {
+  swagger: '2.0',
+  info: {
+    title: 'API V1',
+    version: 'v1'
+  },
+  basePath: '/api/v1',
+  definitions: {
+    casino: CASINO_SCHEMA,
+    casinos_collection: CASINOS_COLLECTION_SCHEMA,
+    casino_resource: CASINO_RESOURCE_SCHEMA,
+  }
+}
