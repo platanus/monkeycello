68a929bfdac88a0ad4e1eab4766fb3358527b694
Alejandra Rojas <alejandra@platan.us>
2020-06-03 16:48:08 -0400

    feat(casino-form): add inputs+ button

--- app/javascript/components/casino-form.vue
+++ app/javascript/components/casino-form.vue
@@ -1,5 +1,46 @@
 <template>
   <div>
-    Casino form component
+    <h1>Casino form component</h1>
+    <h2> {{ title }} </h2>
+    <label for="name">Nombre</label>
+    <input
+      id="name"
+      v-model="casinoForm.name"
+    >
+    <label for="monkeys">Cantidad de monkeys</label>
+    <input
+      id="monkeys"
+      type="number"
+      v-model="casinoForm.monkeys"
+    >
+    <label for="bananas">Bananas iniciales</label>
+    <input
+      id="bananas"
+      type="number"
+      v-model="casinoForm.bananas"
+    >
+    <button @click="createCasino">
+      Crear
+    </button>
+    {{ casinoForm.bananas }}
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      title: 'Crear casino',
+      casinoForm: {
+        name: '',
+        monkeys: '',
+        bananas: '',
+      },
+    };
+  },
+  methods: {
+    createCasino() {
+      console.log('Me clickearon');
+    },
+  },
+};
+</script>
