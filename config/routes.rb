3c3abdcca4602cbbdcfcacb1bf2c2fd1d381f3ea
Alejandra Rojas <alejandra@platan.us>
2020-06-10 17:29:38 -0400

    feat(api): power_api generate controller casino

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-scope path: '/api' do
-api_version(module: 'Api::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
-end
-end
+  scope path: '/api' do
+    api_version(module: 'Api::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
+      resources :casinos
+    end
+  end
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
   scope path: '/api' do
     api_version(module: "Api::V1", path: { value: "v1" }, defaults: { format: 'json' }) do
+      resources :casinos
     end
   end
   devise_for :admin_users, ActiveAdmin::Devise.config
