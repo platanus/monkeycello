b46194d519916cc88fb20ad7d2ea95a6a61f0760
Alejandra Rojas <alejandra@platan.us>
2020-06-18 23:52:08 -0400

    feat(): add table with monkeys info

--- app/javascript/components/casino.vue
+++ app/javascript/components/casino.vue
@@ -1,7 +1,28 @@
 <template>
   <div>
     <h1>Casino component</h1>
-    {{ monkeys }}
+    <table>
+      <tr>
+        <th>
+          ID
+        </th>
+        <th>
+          Nombre
+        </th>
+        <th>
+          Bananas
+        </th>
+      </tr>
+      <tbody>
+        <tr
+          v-for="monkey in monkeys"
+          :key="monkey.id"
+        >
+          <td>{{ monkey.id }}</td>
+          <td>{{ monkey.attributes.name }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
@@ -22,7 +43,7 @@ export default {
   mounted() {
     axios
       .get(`/api/v1/casinos/${this.casinoId}/monkeys`)
-      .then(response => (this.monkeys = response.data.data));
+      .then((response) => (this.monkeys = response.data.data));
   },
   methods: {
   },
