b57e4ea683550b41ed58292a3eaba4bdd9846f5f
Alejandra Rojas <alejandra@platan.us>
2020-06-22 12:15:25 -0400

    feat(new bet): add button to show winner monkey and update bananas

--- app/javascript/api/bets.js
+++ app/javascript/api/bets.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+function getBetAttributes(data) {
+  return data.attributes;
+}
+
 export default function postBet(casinoId) {
   return axios
     .post(`/api/v1/casinos/${casinoId}/bets`)
-    .then((response) => (response.data.data));
+    .then((response) => (getBetAttributes(response.data.data)));
 }
--- app/javascript/components/casino.vue
+++ app/javascript/components/casino.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h1>Casino component</h1>
+    <button
+      v-if="thereIsWinner"
+      @click="showWinnerClick"
+    >
+      Mostrar ganador
+    </button>
+    {{ this.monkeys }}
+    <p v-if="showWinner">
+      ID Ganador: {{ lastWinner }}
+    </p>
     <button @click="bet">
       Apostar
     </button>
@@ -43,6 +53,9 @@ export default {
   data() {
     return {
       monkeys: null,
+      lastWinner: null,
+      thereIsWinner: false,
+      showWinner: false,
     };
   },
   mounted() {
@@ -50,7 +63,16 @@ export default {
   },
   methods: {
     bet() {
-      postBet(this.casinoId);
+      postBet(this.casinoId).then((response) => {
+        this.thereIsWinner = true;
+        this.showWinner = false;
+        this.lastWinner = response.winner_id;
+      });
+    },
+    showWinnerClick() {
+      this.thereIsWinner = false;
+      this.showWinner = true;
+      getMonkeys(this.casinoId).then((response) => (this.monkeys = response));
     },
   },
 };
