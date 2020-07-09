6ee4c91b3462ec493a05c7277f91a371dc91e07f
Alejandra Rojas <alejandra@platan.us>
2020-07-09 16:12:21 -0400

    feat(vuex): get monkeys and post bet from store

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,11 @@
 import Vue from 'vue/dist/vue.esm';
 import '../css/application.css';
 import Casino from '../components/casino.vue';
+import store from './store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+    store,
     el: '#app',
     data: {
       message: 'Victoria!',
